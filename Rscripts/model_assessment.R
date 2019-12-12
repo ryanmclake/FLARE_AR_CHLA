1ec0d8de7198b4e3667093aaccdddeea29accbfc
wwoel <wwoelmer@vt.edu>
2019-12-12 10:54:18 -0500

    added some na.rm code to make function more robust

--- Rscripts/model_assessment.R
+++ Rscripts/model_assessment.R
@@ -11,8 +11,8 @@ model_metrics <- function(sim, obs){
   RMSE <- rmse(sim, obs)
   NSE <- NSE(sim, obs)
   KGE <- KGE(sim, obs)
-  bias <- mean(residual)  #mean residual
-  bias_sd <- sd(sim)/sd(obs)  # bias calculations for mean and median of both weeks (1 is non-biased, <1 means model is smoothing, >1 means model is predicting more fluctuations than are observed)
+  bias <- mean(residual, na.rm = TRUE)  #mean residual
+  bias_sd <- sd(sim, na.rm = TRUE)/sd(obs, na.rm = TRUE)  # bias calculations for mean and median of both weeks (1 is non-biased, <1 means model is smoothing, >1 means model is predicting more fluctuations than are observed)
   R2_1_1 <- 1 - sum(residual^2)/sum(data_minusglobamean^2)
   
   x <- summary(lm(sim ~ obs))
