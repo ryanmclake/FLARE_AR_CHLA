9d0adeecb899c0818cc1dac55ea5115a831e646a
wwoel <wwoelmer@vt.edu>
2020-01-23 09:48:34 -0500

    minor changes in data format to read in data tha that has been qaqc'ed by temp_oxy_chla_qaqc.R

--- Rscripts/extract_EXOchl_chain_dailyavg.R
+++ Rscripts/extract_EXOchl_chain_dailyavg.R
@@ -3,18 +3,18 @@
 
 
 extract_chla_chain_dailyavg <- function(fname = catwalk_fname,full_time,depths = modeled_depths,observed_depths_chla_fdom= observed_depths_chla_fdom,input_tz = 'EST5EDT', output_tz = 'GMT'){
-  d <- read.csv(fname, skip =3, na.strings = 'NAN', stringsAsFactors = FALSE)
-  d_names <- read.csv(fname, skip =1)
-  names(d) <- names(d_names)
+  d <- read.csv(fname)
+  #d_names <- read.csv(fname, skip =1)
+  #names(d) <- names(d_names)
   
-  d$date <- date(d$TIMESTAMP)
+  d$date <- date(d$DateTime)
   Chla_obs <- array(NA,dim=c(length(full_time),2))
   
   
   for (i in 1:length(full_time)) {
     temp <- subset(d, d$date==date(full_time)[i])
-    temp1 <- temp %>% mutate(chl_avg = mean(Chla_1, na.rm = TRUE))
-    Chla_obs[i,1] <- temp1[1, 41]
+    temp1 <- temp %>% mutate(chl_avg = mean(EXOChla_ugL_1, na.rm = TRUE))
+    Chla_obs[i,1] <- temp1[1, 48]
     
        }
   return(list(Chla_obs = Chla_obs, depths = depths))
