fafa95b83066396b38c17e2067bea97b2e71d30f
wwoel <wwoelmer@vt.edu>
2020-02-17 14:44:23 -0500

    qaqc file?

---
+++ Rscripts/extract_EXOchl_chain_dailyavg_QAQCed_data.R
@@ -0,0 +1,24 @@
+
+# script to extract EXO sonde chl data for the forecast time period and calculate daily averages
+
+
+extract_chla_chain_dailyavg <- function(fname = catwalk_fname,full_time,depths = modeled_depths,observed_depths_chla_fdom= observed_depths_chla_fdom,input_tz = 'EST5EDT', output_tz = 'GMT'){
+  d <- read.csv(fname)
+  #d_names <- read.csv(fname, skip =1)
+  #names(d) <- names(d_names)
+  
+  d$date <- date(d$DateTime)
+  Chla_obs <- array(NA,dim=c(length(full_time),2))
+  
+  
+  for (i in 1:length(full_time)) {
+    temp <- subset(d, d$date==date(full_time)[i])
+    temp1 <- temp %>% mutate(chl_avg = mean(EXOChla_ugL_1, na.rm = TRUE))
+    Chla_obs[i,1] <- temp1[1, 4]
+    
+       }
+  return(list(Chla_obs = Chla_obs, depths = depths))
+}
+
+
+
