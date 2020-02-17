357e8aab029cba541e5e7f05415ae99cb0824d1a
wwoel <wwoelmer@vt.edu>
2020-02-17 14:40:18 -0500

    new script to make plots and calculate forecast metrics from daily forecst output

---
+++ Rscripts/plot_assess_daily_model.R
@@ -0,0 +1,279 @@
+
+library(ggplot2)
+
+folder <- "C:/Users/wwoel/Desktop/FLARE_AR_CHLA"
+timestep <- 'daily'
+forecast_folder <- paste0(folder, "/FCR_forecasts", '/', timestep, '/relhum_training_spinup_Feb0420')
+
+setwd(forecast_folder)
+
+# code to read in the individual forecast files named for the day on which the forecast is made
+myfiles_forecast <- list.files(path = forecast_folder, pattern = paste0('*', timestep, ".csv"))
+dataset_forecast <- read.csv(paste0(forecast_folder, "/", myfiles_forecast[1]))
+
+# read in files
+for (i in 2:length(myfiles_forecast)) {
+  temp_2 <- read.csv(paste0(forecast_folder,"/", myfiles_forecast[i]))
+  dataset_forecast <- rbind(dataset_forecast, temp_2)
+}
+
+# some data arranging
+stuff <- dataset_forecast 
+stuff$forecast_date <- as.Date(stuff$forecast_date, "%Y-%m-%d")
+stuff <- stuff[order(stuff$forecast_date),]
+stuff$week <- as.factor(stuff$day_in_future)
+stuff$forecast_run_day <- as.Date(stuff$forecast_run_day, "%Y-%m-%d")
+
+dates <- unique(stuff$forecast_run_day)
+
+# bring in the null model data
+myfiles_null <- list.files(path = paste0(forecast_folder, '/null_ensemble'), pattern = paste0('*', 'null_summary', ".csv"))
+dataset_null <- read.csv(paste0(paste0(forecast_folder, '/null_ensemble'), "/", myfiles_null[1]))
+# shit I forgot to archive the forecast run day within the null model file
+dataset_null$forecast_run_day <- NA
+dataset_null$day_in_future <- NA
+
+# read in files
+for (i in 2:length(myfiles_null)) {
+  temp <- read.csv(paste0(forecast_folder, '/null_ensemble',"/", myfiles_null[i]))
+  temp$day_in_future <- seq(1,16)
+  temp$forecast_run_day <- dates[i]
+  dataset_null <- rbind(dataset_null, temp)
+  }
+
+
+dataset_null$forecast_run_day[1:16] <- as.Date('2018-12-15')
+dataset_null$forecast_run_day <- as.Date(dataset_null$forecast_run_day, '1970-01-01')
+dataset_null$day_in_future[1:16] <- seq(1:16)
+
+#remove spin up dates, so anything before Dec 31, 2018
+stuff <- stuff[stuff$forecast_run_day>'2018-12-31',]
+dataset_null <- dataset_null[dataset_null$forecast_run_day>'2018-12-31',]
+
+# separate into forecast horizon for individual analysis
+for (i in 1:16) { # change the 16 to whatever number of timesteps you have
+  temp <- stuff[stuff$day_in_future==i,]
+  write.csv(temp, paste0(forecast_folder, '/day_', i, '.csv'), row.names = FALSE)
+}
+
+for (i in 1:16) { # change the 16 to whatever number of timesteps you have
+  temp <- dataset_null[dataset_null$day_in_future==i,]
+  write.csv(temp, paste0(forecast_folder, '/day_', i, '_null.csv'), row.names = FALSE)
+}
+
+
+# loop to make figures at each time step
+#for(i in 1:16){
+  
+  # read in csv of each forecast horizon
+  temp <- read.csv(paste0(forecast_folder, '/day_', i, '.csv'))
+  temp$forecast_date <- as.Date(temp$forecast_date)
+
+  #create and save figure with forecast mean, confidence intervals, and obs chl
+ # png(paste0(forecast_folder, '/Forecast_day_', i, '.png'), width = 1100, height = 800)
+    print(ggplot(temp, aes(forecast_date, forecast_mean_chl)) +
+    geom_line(size = 2) +
+    geom_point(aes(forecast_date, obs_chl_EXO), size = 4, stroke = 0, shape = 19, color = 'green4') +
+    geom_ribbon(aes(ymin = forecast_CI95_lower, ymax = forecast_CI95_upper), fill = 'forestgreen', linetype = 2, alpha = 0.2) +
+    xlab('Date') +
+    ylab('Chlorophyll a (μg/L)') +
+    #geom_vline(xintercept = as.numeric(as.Date("2019-02-28", "%Y-%m-%d")), color = 'blue', size = 1.5) +
+    #geom_vline(xintercept = as.numeric(as.Date("2019-03-20", "%Y-%m-%d")), color = 'blue', size = 1.5) +
+    theme(axis.text.x = element_text(size = 35),
+          axis.text.y = element_text(size = 50),
+          axis.title.x = element_text(size =45),
+          axis.title.y = element_text(size = 45),
+          legend.title = element_text(size = 35),
+          legend.text = element_text(size = 30),
+          panel.grid.major = element_blank(),
+          legend.position = 'right',
+          panel.grid.minor = element_blank()))
+  #dev.off()
+#}
+
+
+
+# create table that null and forecast metrics will be writte into
+metrics <- array(NA, dim = c(8, 4))
+row.names(metrics) <- c('RMSE', "NSE", 'KGE', 'bias', 'bias_sd', 'R2_1_1', 'coefficient of determination', 'corr_p')
+colnames(metrics) <- c('forecast', 'null', 'forecast_nonbloom', 'null_nonbloom')
+
+metrics_overtime <- array(NA, dim = c(16,17))
+colnames(metrics_overtime) <- c('day_in_future','RMSE_null', 'RMSE_forecast',"RMSE_null_nonbloom", 'RMSE_forecast_nonbloom', 
+                                                'R2_null', 'R2_forecast',"R2_null_nonbloom", 'R2_forecast_nonbloom', 
+                                                'RMSE_forecast_weekly', 'R2_forecast_weekly', 'RMSE_forecast_weekly_nonbloom', 'R2_forecast_weekly_nonbloom',
+                                                'RMSE_null_weekly', 'R2_null_weekly', 'RMSE_null_weekly_nonbloom', 'R2_null_weekly_nonbloom')
+for (i in 1:16) {
+  # read in csv of each forecast horizon
+  temp <- read.csv(paste0(forecast_folder, '/day_', i, '.csv'))
+  temp$forecast_date <- as.Date(temp$forecast_date)
+
+  temp_null <- read.csv(paste0(forecast_folder, '/day_', i, '_null.csv'))
+  temp_null$forecast_run_day <- as.Date(temp_null$forecast_run_day)
+
+  # calculate forecast metrics
+  source(paste0(folder,"/","Rscripts/model_assessment.R")) # sim, obs
+  
+  forecast <- model_metrics(temp$forecast_mean_chl, temp$obs_chl_EXO)
+  metrics[1,1] <- forecast$RMSE
+  metrics[2,1] <- forecast$NSE
+  metrics[3,1] <- forecast$KGE
+  metrics[4,1] <- forecast$bias
+  metrics[5,1] <- forecast$bias_sd
+  metrics[6,1] <- forecast$R2_1_1
+  metrics[7,1] <- forecast$coeff_determination
+  metrics[8,1] <- forecast$cor_p
+  metrics_overtime[i,3] <- forecast$RMSE
+  metrics_overtime[i,7] <- forecast$coeff_determination
+  
+  null <- model_metrics(temp_null$V1, temp$obs_chl_EXO)
+  metrics[1,2] <- null$RMSE
+  metrics[2,2] <- null$NSE
+  metrics[3,2] <- null$KGE
+  metrics[4,2] <- null$bias
+  metrics[5,2] <- null$bias_sd
+  metrics[6,2] <- null$R2_1_1
+  metrics[7,2] <- null$coeff_determination
+  metrics[8,2] <- null$cor_p
+  metrics_overtime[i,2] <- null$RMSE
+  metrics_overtime[i,6] <- null$coeff_determination
+  metrics_overtime[i,1] <- i
+  
+  non_bloom <- temp[temp$forecast_date< as.Date("2019-07-10") | temp$forecast_date > as.Date("2019-08-10"), ]
+  non_bloom_forecast <- model_metrics(non_bloom$forecast_mean_chl, non_bloom$obs_chl_EXO)
+  metrics[1,3] <- non_bloom_forecast$RMSE
+  metrics[2,3] <- non_bloom_forecast$NSE
+  metrics[3,3] <- non_bloom_forecast$KGE
+  metrics[4,3] <- non_bloom_forecast$bias
+  metrics[5,3] <- non_bloom_forecast$bias_sd
+  metrics[6,3] <- non_bloom_forecast$R2_1_1
+  metrics[7,3] <- non_bloom_forecast$coeff_determination
+  metrics[8,3] <- non_bloom_forecast$cor_p
+  metrics_overtime[i,5] <- non_bloom_forecast$RMSE
+  metrics_overtime[i,9] <- non_bloom_forecast$coeff_determination
+  
+  
+  datamerge_nonbloom <- temp_null[temp_null$forecast_run_day< as.Date("2019-07-10") | temp_null$forecast_run_day > as.Date("2019-08-10"), ]
+  non_bloom_null <- model_metrics(datamerge_nonbloom$V1, non_bloom$obs_chl_EXO)
+  metrics[1,4] <- non_bloom_null$RMSE
+  metrics[2,4] <- non_bloom_null$NSE
+  metrics[3,4] <- non_bloom_null$KGE
+  metrics[4,4] <- non_bloom_null$bias
+  metrics[5,4] <- non_bloom_null$bias_sd
+  metrics[6,4] <- non_bloom_null$R2_1_1
+  metrics[7,4] <- non_bloom_null$coeff_determination
+  metrics[8,4] <- non_bloom_null$cor_p
+  metrics_overtime[i,4] <- non_bloom_null$RMSE
+  metrics_overtime[i,8] <- non_bloom_null$coeff_determination
+  
+  
+  print(paste0('metrics on day ', i))
+  print(metrics)
+  #write.csv(metrics, paste0(forecast_folder, '/ForecastMetrics_day_', i, '.csv'))
+  
+}
+
+
+metrics_overtime <- as.data.frame(metrics_overtime)
+# read in the csv with the weekly metrics saved
+weekly <- read.csv(paste0(folder, '/FCR_forecasts/weekly/weekly_05Feb2020/ForecastMetrics_Weekly.csv'))
+weekly <- weekly[,-1]
+
+metrics_overtime[7,10] <- weekly[1,3] #week1 forecast rmse
+metrics_overtime[7,11] <- weekly[1,7] #week1 forecast r2
+metrics_overtime[7,12] <- weekly[1,5] #week1 nonbloom rmse
+metrics_overtime[7,13] <- weekly[1,9] #week1 r2 nonbloom
+
+metrics_overtime[14,10] <- weekly[2,3] #week2 forecast rmse
+metrics_overtime[14,11] <- weekly[2,7] #week2 forecast r2
+metrics_overtime[14,12] <- weekly[2,5] #week2 nonbloom rmse
+metrics_overtime[14,13] <- weekly[2,9]  #week2 r2 nonbloom
+
+metrics_overtime[7,14] <- weekly[1,2]#week1 null rmse
+metrics_overtime[7,15] <- weekly[1,6] #week1 null r2
+metrics_overtime[7,16] <- weekly[1,4]#week1 nonbloom null rmse
+metrics_overtime[7,17] <- weekly[1,8] #week1 r2 nonbloom null
+
+metrics_overtime[14,14] <- weekly[2,2] #week2 null rmse
+metrics_overtime[14,15] <- weekly[2,6]  #week2 null r2
+metrics_overtime[14,16] <- weekly[2,4]  #week2 nonbloom rmse
+metrics_overtime[14,17] <- weekly[2,8]  #week2 r2 nonbloom
+  
+plot(metrics_overtime$day_in_future, metrics_overtime$RMSE_null, col = 'red', ylim = c(0,11), xlab = 'forecast horizon', ylab = 'RMSE')
+points(metrics_overtime$day_in_future, metrics_overtime$RMSE_forecast, col = 'blue')
+points(metrics_overtime$day_in_future, metrics_overtime$RMSE_forecast_nonbloom, col = 'orange')
+points(metrics_overtime$day_in_future, metrics_overtime$RMSE_null_nonbloom, col = 'purple')
+points(metrics_overtime$day_in_future, metrics_overtime$RMSE_forecast_weekly,  col = 'blue', pch = 15)
+points(metrics_overtime$day_in_future, metrics_overtime$RMSE_forecast_weekly_nonbloom, col = 'orange', pch = 15)
+points(metrics_overtime$day_in_future, metrics_overtime$RMSE_null_weekly, col = 'red', pch = 15)
+points(metrics_overtime$day_in_future, metrics_overtime$RMSE_null_weekly_nonbloom, col = 'purple', pch = 15)
+legend('topleft', c('null', 'forecast', 'null nonbloom', 'forecast nonbloom'), col = c('red', 'blue', 'purple', 'orange'), lty = c(1,1), bty = 'n')
+
+plot(metrics_overtime$day_in_future, metrics_overtime$R2_null, col = 'red', ylim = c(0,1), xlab = 'forecast horizon', ylab = 'Coefficient of Determination')
+points(metrics_overtime$day_in_future, metrics_overtime$R2_forecast, col = 'blue')
+points(metrics_overtime$day_in_future, metrics_overtime$R2_forecast_nonbloom, col = 'orange')
+points(metrics_overtime$day_in_future, metrics_overtime$R2_null_nonbloom, col = 'purple')
+points(metrics_overtime$day_in_future, metrics_overtime$R2_forecast_weekly, col = 'blue', pch = 15)
+points(metrics_overtime$day_in_future, metrics_overtime$R2_forecast_weekly_nonbloom, col = 'orange', pch = 15)
+points(metrics_overtime$day_in_future, metrics_overtime$R2_null_weekly, col = 'red', pch = 15)
+points(metrics_overtime$day_in_future, metrics_overtime$R2_null_weekly_nonbloom, col = 'purple', pch = 15)
+legend('topright', c('null', 'forecast', 'null nonbloom', 'forecast nonbloom'), col = c('red', 'blue', 'purple', 'orange'), lty = c(1,1), bty = 'n')
+
+
+################################################################################################################################################################
+#### calculate ensemble metrics ########################################################################################################################################
+################################################################################################################################################################
+
+# smaller score corresponds to a better forecast
+
+library(scoringRules)
+
+# read in forecast distribution and observed data
+ensemble_forecast <- list.files(path = forecast_folder, pattern = paste0('*', '_chla_daily_ensembles', ".csv"))
+ensemble_forecast <- ensemble_forecast[-c(1:9)] # start the list on 01-01-2019
+obs_file <- list.files(path = forecast_folder, pattern = paste0('day_', '*', '.csv'))
+nsteps <- 16
+crps_forecast <- array(NA, dim = c(length(ensemble_forecast), 2, nsteps))
+
+for (i in 1:length(ensemble_forecast)) {
+  temp <- read.csv(paste0(forecast_folder,'/', ensemble_forecast[i]))
+  
+  for (j in 1:16) {
+    obs <- read.csv(paste0(forecast_folder, '/day_', j, '.csv'))
+    obs_chl <- obs$obs_chl_EXO[i]
+    dat <- as.numeric(temp[j,])
+    crps_forecast[i,1,j] <- crps_sample(y = obs_chl, dat = dat)  
+  }
+
+}
+
+plot(crps_forecast[,1,1], type ='l')
+
+for (i in 1:16) {
+  x <- mean(crps_forecast[,1,i], na.rm = TRUE)
+  print(paste0('forecast crps on day ', i,' ',  x))
+}
+
+
+ensemble_null <- list.files(path = paste0(forecast_folder, '/null_ensemble'), pattern = paste0('*', '_null_ensembles', '.csv'))
+ensemble_null <- ensemble_null[-c(1:9)]
+crps_null <- array(NA, dim = c(length(ensemble_null), 2, nsteps))
+
+for (i in 1:length(ensemble_null)) {
+  temp <- read.csv(paste0(forecast_folder,'/null_ensemble/', ensemble_null[i]))
+  
+  for (j in 1:16) {
+    obs <- read.csv(paste0(forecast_folder, '/day_', j, '.csv'))
+    obs_chl <- obs$obs_chl_EXO[i]
+    dat <- as.numeric(temp[j,])
+    crps_null[i,1,j] <- crps_sample(y = obs_chl, dat = dat)  
+  }
+  
+}
+
+plot(crps_null[,1,1], type = 'l')
+
+for (i in 1:16) {
+  x <- mean(crps_null[,1,i], na.rm = TRUE)
+  print(paste0('null crps on day ', i,' ',  x))
+}
