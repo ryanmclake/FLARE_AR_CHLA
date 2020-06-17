d805005e3fdaf0e34a6a9e01e5de654664b7a6c4
wwoel <wwoelmer@vt.edu>
2020-06-17 11:08:00 -0400

    added code which creates SI fig showing time series of chl for training and forecast period

--- Rscripts/summary_stats_train_forecast_chla.R
+++ Rscripts/summary_stats_train_forecast_chla.R
@@ -4,19 +4,22 @@ library(tidyverse)
 
 
 folder <- "C:/Users/wwoel/Desktop/FLARE_AR_CHLA"
-timestep <- 'daily'
+timestep <- 'weekly'
 setwd(folder)
 
 # training dataset May-Oct 2013-2016
 train <- read.csv('./data_arima_WW.csv')
+train$Date <- as.Date(train$Date)
 train <- train %>% mutate(Chla = Chla_sqrt^2) %>% 
   mutate(Chla_EXO = ((Chla + 0.0308)/0.55))
+train <- train %>% select(Date, Chla_EXO)
+
 median(train$Chla_EXO)
 sd(train$Chla_EXO)
-plot(train$Date, train$Chla_EXO)
+plot(train$Date, train$Chla_EXO, xlab = 'Date', ylab = 'Chl-a (ug/L)')
 
 # and over the forecasting period, Jan 2019-Dec 2019
-forecast_folder <- paste0(folder, "/FCR_forecasts", '/', timestep, '/relhum_training_spinup_Feb0420')
+forecast_folder <- paste0(folder, "/FCR_forecasts", '/', timestep, '/weekly_dischargeforecast_Apr2020')
 setwd(forecast_folder)
 
 # code to read in the individual forecast files named for the day on which the forecast is made
@@ -35,7 +38,18 @@ stuff$forecast_date <- as.Date(stuff$forecast_date, "%Y-%m-%d")
 stuff <- stuff[order(stuff$forecast_date),]
 stuff$week <- as.factor(stuff$day_in_future)
 stuff$forecast_run_day <- as.Date(stuff$forecast_run_day, "%Y-%m-%d")
-#remove spin up dates, so anything before Dec 31, 2018
+
+stuff <- stuff %>% select(forecast_date, obs_chl_EXO)
+colnames(stuff) <- c('Date', 'Chla_EXO')
+
+all <- rbind(train, stuff)
+png('C:/Users/wwoel/Dropbox/Thesis/Figures/arima/SI_Fig_chl_timeseries.png', width = 1100, height = 800)
+par(mar= c(5.1, 5.1, 4.1, 2.1))
+plot(all$Date, all$Chla_EXO, xlab = 'Date', ylab = 'Chl-a (μg/L)', cex.axis = 2, cex.main = 2, cex.lab = 2, cex = 1.5)
+dev.off()
+
+
+# for summery states, remove spin up dates, so anything before Dec 31, 2018
 stuff <- stuff[stuff$forecast_run_day>'2018-12-31',]
 
 median(stuff$obs_chl_EXO)
