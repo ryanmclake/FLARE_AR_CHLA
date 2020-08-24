e1eec7162d49e7b4fd668de782523b318acefcdc
wwoel <wwoelmer@vt.edu>
2020-08-24 16:49:35 -0400

    updated with newer forecasts, cleaned up naming conventions

--- Rscripts/summary_stats_train_forecast_chla.R
+++ Rscripts/summary_stats_train_forecast_chla.R
@@ -4,7 +4,8 @@ library(tidyverse)
 
 
 folder <- "C:/Users/wwoel/Desktop/FLARE_AR_CHLA"
-timestep <- 'weekly'
+timestep <- '14day'
+sim_name <- '22Jul2020'
 setwd(folder)
 
 # training dataset May-Oct 2013-2016
@@ -18,8 +19,8 @@ median(train$Chla_EXO)
 sd(train$Chla_EXO)
 plot(train$Date, train$Chla_EXO, xlab = 'Date', ylab = 'Chl-a (ug/L)')
 
-# and over the forecasting period, Jan 2019-Dec 2019
-forecast_folder <- paste0(folder, "/FCR_forecasts", '/', timestep, '/weekly_dischargeforecast_Apr2020')
+# and over the forecasting period, Jan 2019-Jun 2020
+forecast_folder <- paste0(folder, "/FCR_forecasts", '/', timestep, '/', sim_na)
 setwd(forecast_folder)
 
 # code to read in the individual forecast files named for the day on which the forecast is made
@@ -39,8 +40,20 @@ stuff <- stuff[order(stuff$forecast_date),]
 stuff$week <- as.factor(stuff$day_in_future)
 stuff$forecast_run_day <- as.Date(stuff$forecast_run_day, "%Y-%m-%d")
 
-stuff <- stuff %>% select(forecast_date, obs_chl_EXO)
+stuff <- stuff %>% select(forecast_run_day, obs_chl_EXO)
 colnames(stuff) <- c('Date', 'Chla_EXO')
+
+# for summary stats, remove spin up dates, so anything before Dec 31, 2018
+stuff <- stuff[stuff$Date>'2018-12-31',]
+
+median(stuff$Chla_EXO)
+sd(stuff$Chla_EXO)
+max(stuff$Chla_EXO)
+
+plot(stuff$Date, stuff$Chla_EXO, xlim = c(as.Date('2020-01-01'), as.Date('2020-05-01')), type = 'l')
+abline(h = 17.1)
+abline(v = as.Date('2020-03-16'))
+abline(v = as.Date('2020-04-23'))
 
 all <- rbind(train, stuff)
 png('C:/Users/wwoel/Dropbox/Thesis/Figures/arima/SI_Fig_chl_timeseries.png', width = 1100, height = 800)
@@ -49,11 +62,6 @@ plot(all$Date, all$Chla_EXO, xlab = 'Date', ylab = 'Chl-a (μg/L)', cex.axis = 2
 dev.off()
 
 
-# for summery states, remove spin up dates, so anything before Dec 31, 2018
-stuff <- stuff[stuff$forecast_run_day>'2018-12-31',]
-
-median(stuff$obs_chl_EXO)
-sd(stuff$obs_chl_EXO)
 
 
 
