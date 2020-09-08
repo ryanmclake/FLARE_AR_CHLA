56f9f15dac7081757c01c20f1d6d05df571fdcd3
wwoel <wwoelmer@vt.edu>
2020-09-08 13:21:24 -0400

    modified code to deal with input filing naming inconsistencies, figure formating changes

--- Rscripts/uncertainty_analysis_arima_daily.R
+++ Rscripts/uncertainty_analysis_arima_daily.R
@@ -1,23 +1,21 @@
 # script to analyze different sources of uncertainty for arima forecast output
-
-library(scales)
 library(rsq)
 library(tidyverse)
 library(Metrics)
 reference_tzone <- "GMT"
 
 #set the location of the forecasts
-forecast_folder <- "C:/Users/wwoel/Desktop/FLARE_AR_CHLA"
-setwd(forecast_folder)
-sim_folder <- paste0(forecast_folder, '/FCR_forecasts/daily/relhum_training_spinup_Feb0420')
+folder <- "C:/Users/wwoel/Desktop/FLARE_AR_CHLA"
+sim_name <- '17Jul2020'
+forecast_folder <- paste0(folder, "/FCR_forecasts", '/1day/', sim_name)
 
 # the year's forecasts with all uncertainties 'on' in the forecast output
-myfiles <- list.files(path = sim_folder, pattern = "*daily.csv")
-dataset <- read.csv(paste0(sim_folder, "/", myfiles[1]))
+myfiles <- list.files(path = forecast_folder, pattern = "*1day.csv")
+dataset <- read.csv(paste0(forecast_folder, "/", myfiles[1]))
 
 # read in files
 for (i in 2:length(myfiles)) {
-  temp <- read.csv(paste0(sim_folder, "/", myfiles[i]))
+  temp <- read.csv(paste0(forecast_folder, "/", myfiles[i]))
   dataset <- rbind(dataset, temp)
 }
 
@@ -25,38 +23,38 @@ for (i in 2:length(myfiles)) {
 
 # read in each batch on uncertainty files
 ################ uncertainty 2 = process uncertainty
-myfiles_2 <- list.files(path = paste0(sim_folder, "/uncertainty_2_process/"), pattern = "*daily.csv")
-dataset_2 <- read.csv(paste0(sim_folder, "/uncertainty_2_process/", myfiles_2[1]))
+myfiles_2 <- list.files(path = paste0(forecast_folder, "/uncertainty_2_process/"), pattern = "*1day.csv")
+dataset_2 <- read.csv(paste0(forecast_folder, "/uncertainty_2_process/", myfiles_2[1]))
 # read in files
 for (i in 2:length(myfiles_2)) {
-  temp <- read.csv(paste0(sim_folder, "/uncertainty_2_process/", myfiles_2[i]))
+  temp <- read.csv(paste0(forecast_folder, "/uncertainty_2_process/", myfiles_2[i]))
   dataset_2 <- rbind(dataset_2, temp)
 }
 
 ############### uncertainty 3 = weather uncertainty
-myfiles_3 <- list.files(path = paste0(sim_folder, "/uncertainty_3_weather/"), pattern = "*daily.csv")
-dataset_3 <- read.csv(paste0(sim_folder, '/uncertainty_3_weather/', myfiles_3[1]))
+myfiles_3 <- list.files(path = paste0(forecast_folder, "/uncertainty_3_weather/"), pattern = "*1day.csv")
+dataset_3 <- read.csv(paste0(forecast_folder, '/uncertainty_3_weather/', myfiles_3[1]))
 # read in files
 for (i in 2:length(myfiles_3)) {
-  temp <- read.csv(paste0(sim_folder, "/uncertainty_3_weather/", myfiles_3[i]))
+  temp <- read.csv(paste0(forecast_folder, "/uncertainty_3_weather/", myfiles_3[i]))
   dataset_3 <- rbind(dataset_3, temp)
 }
 
 ############### uncertainty 4 = initial condition uncertainty
-myfiles_4 <- list.files(path = paste0(sim_folder, "/uncertainty_4_initial_condition/"), pattern = "*daily.csv")
-dataset_4 <- read.csv(paste0(sim_folder, "/uncertainty_4_initial_condition/", myfiles_4[1]))
+myfiles_4 <- list.files(path = paste0(forecast_folder, "/uncertainty_4_initial_condition/"), pattern = "*1day.csv")
+dataset_4 <- read.csv(paste0(forecast_folder, "/uncertainty_4_initial_condition/", myfiles_4[1]))
 # read in files
 for (i in 2:length(myfiles_4)) {
-  temp <- read.csv(paste0(sim_folder, "/uncertainty_4_initial_condition/", myfiles_4[i]))
+  temp <- read.csv(paste0(forecast_folder, "/uncertainty_4_initial_condition/", myfiles_4[i]))
   dataset_4 <- rbind(dataset_4, temp)
 }
 
 ############### uncertainty 5 = parameter uncertainty
-myfiles_5 <- list.files(path = paste0(sim_folder, "/uncertainty_5_parameter"), pattern = "*daily.csv")
-dataset_5 <- read.csv(paste0(sim_folder, "/uncertainty_5_parameter/", myfiles_5[1]))
+myfiles_5 <- list.files(path = paste0(forecast_folder, "/uncertainty_5_parameter"), pattern = "*1day.csv")
+dataset_5 <- read.csv(paste0(forecast_folder, "/uncertainty_5_parameter/", myfiles_5[1]))
 # read in files
 for (i in 2:length(myfiles_5)) {
-  temp <- read.csv(paste0(sim_folder, "/uncertainty_5_parameter/", myfiles_5[i]))
+  temp <- read.csv(paste0(forecast_folder, "/uncertainty_5_parameter/", myfiles_5[i]))
   dataset_5 <- rbind(dataset_5, temp)
 }
 
@@ -113,6 +111,9 @@ uncert <- uncert %>% mutate(process_prop = process_variance/(process_variance + 
 # dataframe with just proportions
 uncert_prop <- uncert %>% select(forecast_date, forecast_run_day, day_in_future, process_prop, weather_prop, IC_prop, parameter_prop, total_var)
 
+# remove dates after 15-Aug
+uncert_prop <- uncert_prop[uncert_prop$forecast_run_day<"2020-08-15",]
+
 ## put into long format for easy plotting
 uncert_proportion_long <- uncert_prop  %>% 
   gather(variable, measurement, process_prop:total_var)
@@ -132,10 +133,10 @@ for (i in 1:16) { # change the 16 to whatever number of timesteps you have
 
 
 # pretty time series with geom_area()
-for (i in 1:16) {
+for (i in 1:14) {
   temp <- uncert_proportion_long[uncert_proportion_long$day_in_future==i,]
   temp <- temp[!temp$variable=='total_var',]
-  png(paste0('C:/Users/wwoel/Dropbox/Thesis/Figures/arima/Daily_Day',i, '_Uncertainty_TimeSeries.png'), width = 1200, height = 785)
+  png(paste0(forecast_folder, '/Daily_Day',i, '_Uncertainty_TimeSeries.png'), width = 1200, height = 785)
   print(ggplot(temp, aes(x = forecast_date, y = measurement, fill = variable)) +geom_area() + ylim(0,1.1) +
           xlab('Date') +
           ylab('Proportion of Variance') +
@@ -162,6 +163,40 @@ for (i in 1:16) {
 
 ###############################################
 ## Figure 6 with total variance overplotted
+# Day 1
+
+temp <- uncert_proportion_long[uncert_proportion_long$day_in_future==1,]
+temp <- temp[temp$forecast_run_day>'2019-01-01',]
+var <- temp[temp$variable=='total_var',]
+temp <- temp[!temp$variable=='total_var',]
+
+p <- ggplot() 
+p <- p + geom_area(data = temp, aes(x = forecast_date, y = measurement, fill = variable))+ 
+  xlab('Date') +
+  ylab('Proportion of Variance') +
+  scale_fill_manual(breaks = c('IC', 'parameter', 'process', 'weather'),
+                    values = c('#92D050', '#660066', '#C55A11', '#FFC000'),
+                    name = "Uncertainty Type") +
+  ggtitle(paste0('Daily Forecast, Day 1')) +
+  scale_x_date(labels = date_format('%b'), expand = c(0,0), date_breaks = '3 months') +
+  theme_bw() +
+  theme(axis.text.x = element_text(size = 35),
+        axis.text.y = element_text(size = 35),
+        axis.title.x = element_text(size =35),
+        axis.title.y = element_text(size = 35),
+        legend.title = element_text(size = 35),
+        legend.text = element_text(size = 30),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        plot.title = element_text(size = 50))
+p <- p + geom_line(data = var, aes(x = forecast_date, y = measurement), lwd = 1.5) +
+  scale_y_continuous(expand = c(0,0), sec.axis = sec_axis(~., name = expression (paste("Total Variance (",~μg/L^2,")"  )))) 
+png(paste0(forecast_folder, '/Daily_Day',1, '_Uncertainty_Variance_TimeSeries.png'), width = 1200, height = 785)
+p
+dev.off()
+
+
+
 # Day 7
 
 temp <- uncert_proportion_long[uncert_proportion_long$day_in_future==7,]
@@ -176,7 +211,7 @@ p <- p + geom_area(data = temp, aes(x = forecast_date, y = measurement, fill = v
                     values = c('#92D050', '#660066', '#C55A11', '#FFC000'),
                     name = "Uncertainty Type") +
   ggtitle(paste0('Daily Forecast, Day 7')) +
-  scale_x_date(labels = date_format('%b'), expand = c(0,0)) +
+  scale_x_date(labels = date_format('%b'), expand = c(0,0), date_breaks = '3 months') +
   theme_bw() +
   theme(axis.text.x = element_text(size = 35),
         axis.text.y = element_text(size = 35),
@@ -189,7 +224,7 @@ p <- p + geom_area(data = temp, aes(x = forecast_date, y = measurement, fill = v
         plot.title = element_text(size = 50))
 p <- p + geom_line(data = var, aes(x = forecast_date, y = measurement), lwd = 1.5) +
   scale_y_continuous(expand = c(0,0), sec.axis = sec_axis(~., name = expression (paste("Total Variance (",~μg/L^2,")"  )))) 
-png(paste0('C:/Users/wwoel/Dropbox/Thesis/Figures/arima/Daily_Day',7, '_Uncertainty_Variance_TimeSeries.png'), width = 1200, height = 785)
+png(paste0(forecast_folder, '/Daily_Day',7, '_Uncertainty_Variance_TimeSeries.png'), width = 1200, height = 785)
 p
 dev.off()
 
@@ -208,7 +243,7 @@ p <- p + xlab('Date') +
                     values = c('#92D050', '#660066', '#C55A11', '#FFC000'),
                     name = "Uncertainty Type") +
   ggtitle(paste0('Daily Forecast, Day 14')) +
-  scale_x_date(labels = date_format('%b'), expand = c(0,0)) +
+  scale_x_date(labels = date_format('%b'), expand = c(0,0), date_breaks = '3 months' ) +
   theme_bw() +
   theme(axis.text.x = element_text(size = 35),
         axis.text.y = element_text(size = 35),
@@ -219,7 +254,7 @@ p <- p + xlab('Date') +
         #panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
         plot.title = element_text(size = 50))
-png(paste0('C:/Users/wwoel/Dropbox/Thesis/Figures/arima/Daily_Day',14, '_Uncertainty_Variance_TimeSeries.png'), width = 1200, height = 785)
+png(paste0(forecast_folder, '/Daily_Day',14, '_Uncertainty_Variance_TimeSeries.png'), width = 1200, height = 785)
 p
 dev.off()
 
@@ -232,10 +267,10 @@ colnames(mean_prop) = c('horizon', 'mean_IC_prop', 'mean_parameter_prop','mean_p
 for (i in 1:16) {
   temp <- uncert_prop[uncert_prop$day_in_future==i,]
   mean_prop[i,1] <- i
-  mean_prop[i, 2] = mean(temp$IC_prop)
-  mean_prop[i, 3] = mean(temp$parameter_prop)
-  mean_prop[i, 4] = mean(temp$process_prop)
-  mean_prop[i, 5] = mean(temp$weather_prop)
+  mean_prop[i, 2] = mean(temp$IC_prop, na.rm = TRUE)
+  mean_prop[i, 3] = mean(temp$parameter_prop, na.rm = TRUE)
+  mean_prop[i, 4] = mean(temp$process_prop, na.rm = TRUE)
+  mean_prop[i, 5] = mean(temp$weather_prop, na.rm = TRUE)
 }
 
 
@@ -253,7 +288,7 @@ ggplot(mean_prop, aes(x = horizon, y = mean_IC_prop)) +
 mean_prop_long <- mean_prop_long[mean_prop_long$horizon<15,]
 
 cols <- c('IC'="#92D050",'parameter'="#660066", 'process' = '#C55A11', 'weather' = '#FFC000')
-png('C:/Users/wwoel/Dropbox/Thesis/Figures/arima/Uncertainty_Bar_AcrossHorizon_Daily.png', width = 800, height = 885)
+png(paste0(forecast_folder, '/Uncertainty_Bar_AcrossHorizon_Daily.png'), width = 800, height = 885)
 ggplot(mean_prop_long, aes(x = horizon, y = measurement, fill = variable)) + 
   geom_bar(stat = 'identity', position= 'stack') +
   scale_fill_manual(breaks = c('IC', 'parameter', 'process', 'weather'),
