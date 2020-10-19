8af8fbecc8074b2611ea4d5497ec229061106681
wwoel <wwoelmer@vt.edu>
2020-10-19 08:51:24 -0400

    updated file locations

--- Rscripts/plot_1day_7day_14day_together_MSfigs.R
+++ Rscripts/plot_1day_7day_14day_together_MSfigs.R
@@ -5,9 +5,9 @@ library(scales)
 
 folder <- "C:/Users/wwoel/Desktop/FLARE_AR_CHLA"
 
-daily <- read.csv(paste0(folder, '/FCR_forecasts/1day/17Jul2020/ForecastMetrics_1dayrun23Sep20.csv'))
-seven_day <- read.csv(paste0(folder, '/FCR_forecasts/7day/weekly_dischargeforecast_Apr2020/ForecastMetrics_7day.csv'))
-fourteen_day <- read.csv(paste0(folder, '/FCR_forecasts/14day/22Jul2020/ForecastMetrics_14day.csv'))
+daily <- read.csv(paste0(folder, '/FCR_forecasts/1day/null_daily/ForecastMetrics_1day.csv'))
+seven_day <- read.csv(paste0(folder, '/FCR_forecasts/7day/null_weekly/ForecastMetrics_7day.csv'))
+fourteen_day <- read.csv(paste0(folder, '/FCR_forecasts/14day/null_fortnightly/ForecastMetrics_14day.csv'))
 
 par(mar = c(5,5,4,2), mfrow = c(2,2))
 plot(daily$day_in_future, daily$RMSE_forecast, col = 'blue', cex = 3, ylim = c(0,11), xlim = c(0,14), main = 'Full-Year',  cex.axis = 2, cex.main = 2, cex.lab = 2, xlab = 'Forecast horizon (days)', ylab = 'RMSE (μg/L)')
@@ -84,7 +84,7 @@ legend('topleft', c('null', 'forecast', 'null nonbloom', 'forecast nonbloom'), c
 
 ##################
 # multi-panel figure
-png(paste0('C:/Users/wwoel/Dropbox/Thesis/Figures/arima/RMSE_fig_1day7day14dayforecasts/RMSE_all_conditions_run23Sep20.png'), width = 1100, height = 800)
+png(paste0('C:/Users/wwoel/Dropbox/Thesis/Figures/arima/RMSE_fig_1day7day14dayforecasts/RMSE_all_conditions_RWnull.png'), width = 1100, height = 800)
 par(mar = c(5,5,4,2), mfrow = c(2,2))
 plot(metrics_overtime$day_in_future, metrics_overtime$daily_nonbloom_null, col = 'red', pch =4, ylim = c(0,11), xlim = c(0,14), main = 'Non-Bloom', cex.main = 2, cex.axis = 2, cex.lab = 2, cex = 3, xlab = 'Forecast horizon (days)', ylab = 'RMSE (μg/L)')
 points(metrics_overtime$day_in_future, metrics_overtime$daily_nonbloom, col = 'blue', cex = 3)
