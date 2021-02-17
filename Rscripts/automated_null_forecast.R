84c6523779915769bd95cf507046382b3997ebd2
wwoel <wwoelmer@vt.edu>
2021-02-17 14:16:35 -0500

    updated forecast initialization

--- Rscripts/automated_null_forecast.R
+++ Rscripts/automated_null_forecast.R
@@ -4,13 +4,19 @@
 folder <- "C:/Users/wwoel/Desktop/FLARE_AR_CHLA"
 data_location <-  "C:/Users/wwoel/Desktop/FLARE_AR_CHLA/SCCData"
 forecast_start_day <-"2019-01-01 00:00:00"
-nmembers <- 420
-timestep <- '7day'
-timestep_numeric <- 7
-timestep_interval <- 7
-max_timestep <- 2 #max propagations of the timestep: daily = 14, weekly = 2, fortnightly = 1
+
+#set up ensembles
+n_ds_members <- 1
+n_met_members <- 21
+n_discharge_members <- 21
+nmembers <- n_ds_members*n_met_members*n_discharge_members
+
+timestep <- '14day'
+timestep_numeric <- 14
+timestep_interval <- 14
+max_timestep <- 1 #max propagations of the timestep: daily = 14, weekly = 2, fortnightly = 1
 max_horizon <- 14 # maximum number of days that are propagated in this forecast (e.g. daily timestep has max_horizon = 14)
-sim_name <- 'null_weekly'
+sim_name <- 'null_daily_12Feb21'
 forecast_location <- paste0(folder, '/FCR_forecasts/', timestep, '/', sim_name)
 
 start_day <- forecast_start_day 
