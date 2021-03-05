142824a451c950bb0ffb77014fd3595a6eaf2316
wwoel <wwoelmer@vt.edu>
2021-03-04 20:14:39 -0500

    update folder locations

--- Rscripts/plot_parameters_fortnightly.R
+++ Rscripts/plot_parameters_fortnightly.R
@@ -9,7 +9,7 @@ reference_tzone <- "GMT"
 #set the location of the forecasts
 folder <- "C:/Users/wwoel/Desktop/FLARE_AR_CHLA"
 timestep <- '14day'
-sim_name <- 'update_bayes_method_Oct_2020'
+sim_name <- 'Mar2021_UC'
 forecast_folder <- paste0(folder, "/FCR_forecasts", '/', timestep, '/', sim_name)
 
 # code to read in the individual forecast files named for the day on which the forecast is made
@@ -50,6 +50,7 @@ stuff$day_in_future <- as.factor(stuff$day_in_future)
 stuff$forecast_run_day <- as.Date(stuff$forecast_run_day, "%Y-%m-%d")
 
 parms <- stuff[!duplicated(stuff$forecast_run_day),]
+
 plot(parms$forecast_date, parms$par1, type = 'l', ylab = 'Intercept')
 plot(parms$forecast_date, parms$par2, type = 'l', ylab = 'Chl parm')
 plot(parms$forecast_date, parms$par3, type = 'l', ylab = 'Discharge parm')
@@ -63,7 +64,7 @@ xmin<-min(parms$forecast_date,na.rm=T)
 xmax<-max(parms$forecast_date,na.rm=T) 
 xseq<-seq.Date(xmin,xmax,by='1 month')
 
-png('C:/Users/wwoel/Dropbox/Thesis/Figures/arima/fortnightly_parameter_timeseries.png', width = 1100, height = 800)
+png(paste0(forecast_folder, '/fortnightly_parameter_timeseries.png'), width = 1100, height = 800)
 par(mfrow = c(2,3), mar=c(9,7,4,1)+.1)
 plot(parms$forecast_run_day, parms$par1, xlab = '', ylab = 'Intercept par', type = 'l', axes = F, cex.lab = 3)
 axis(2, cex.axis = 3, cex.lab = 5)
@@ -114,3 +115,4 @@ dev.off()
 (max(dataset_forecast$par3) - min(dataset_forecast$par3))/mean(dataset_forecast$par3)*100
 (max(dataset_forecast$par4) - min(dataset_forecast$par4))/mean(dataset_forecast$par4)*100
 (max(dataset_forecast$par5) - min(dataset_forecast$par5))/mean(dataset_forecast$par5)*100
+
