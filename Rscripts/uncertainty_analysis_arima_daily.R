f54b2fa3b0b144cb5284742b03e9c3b8498dcc0b
wwoel <wwoelmer@vt.edu>
2021-03-04 20:13:29 -0500

    update folder locations

--- Rscripts/uncertainty_analysis_arima_daily.R
+++ Rscripts/uncertainty_analysis_arima_daily.R
@@ -8,7 +8,7 @@ reference_tzone <- "GMT"
 
 #set the location of the forecasts
 folder <- "C:/Users/wwoel/Desktop/FLARE_AR_CHLA"
-sim_name <- 'update_bayes_method_Oct_2020'
+sim_name <- 'Mar2021_UC'
 forecast_folder <- paste0(folder, "/FCR_forecasts", '/1day/', sim_name)
 
 # the year's forecasts with all uncertainties 'on' in the forecast output
@@ -21,7 +21,8 @@ for (i in 2:length(myfiles)) {
   dataset <- rbind(dataset, temp)
 }
 
-
+# Error in rbind(deparse.level, ...) : numbers of columns of arguments do not match
+# occurs because last file name is the Forecast_Metrics.csv summary file, ignore this error!
 
 # read in each batch on uncertainty files
 ################ uncertainty 2 = process uncertainty
@@ -69,12 +70,38 @@ for (i in 2:length(myfiles_6)) {
   dataset_6 <- rbind(dataset_6, temp)
 }
 
+plot(dataset$forecast_date, dataset$forecast_variance)
+plot(as.Date(dataset_2$forecast_date), dataset_2$forecast_variance, xlim = c(as.Date('2020-06-11'), as.Date('2020-06-25')), type = 'l')
+plot(as.Date(dataset_2$forecast_date), dataset_2$forecast_mean_chl)
+plot(as.Date(dataset_2$forecast_date), dataset_2$obs_chl_EXO)
+hist(dataset_5$forecast_variance)
+ggplot(data = dataset_2, aes(x = as.Date(forecast_date), y = forecast_variance)) +
+  geom_point(aes(col = forecast_run_day)) +
+  xlim(c(as.Date('2020-06-11'), as.Date('2020-06-25'))) +
+  theme(legend.title = element_blank())
+
+ggplot(data = dataset_3, aes(x = as.Date(forecast_date), y = forecast_variance)) +
+  geom_line() +
+  facet_wrap(~day_in_future)
+
+ggplot(data = dataset_4, aes(x = as.Date(forecast_date), y = forecast_variance)) +
+  geom_line() +
+  facet_wrap(~day_in_future)
+
+ggplot(data = dataset_5, aes(x = as.Date(forecast_date), y = forecast_variance)) +
+  geom_line() +
+  facet_wrap(~day_in_future)
+
+ggplot(data = dataset_6, aes(x = as.Date(forecast_date), y = sqrt(forecast_variance)*0.6 + 0.25)) +
+  geom_line() +
+  facet_wrap(~day_in_future)
+
 process <- mean(dataset_2$forecast_variance, na.rm = TRUE)
 weather <- mean(dataset_3$forecast_variance, na.rm = TRUE)
 IC <- mean(dataset_4$forecast_variance, na.rm = TRUE)
 parameter <- mean(dataset_5$forecast_variance, na.rm = TRUE)
-
-all <- process+weather+IC+parameter
+discharge <- mean(dataset_6$forecast_variance, na.rm = TRUE)
+all <- process+weather+IC+parameter+discharge
 
 # what amount of variance are we not accounting for in our uncertainty analysis?
 (mean(dataset$forecast_variance, na.rm = TRUE) - all)/mean(dataset$forecast_variance, na.rm = TRUE)
@@ -116,6 +143,13 @@ uncert <- left_join(uncert, dataset_5)
 uncert <- left_join(uncert, dataset_6)
 uncert <- left_join(uncert, dataset)
 
+uncert_untransform <- uncert %>% #try undoing the CTD/EXO sqrt transformation
+  mutate(process_variance = (sqrt(process_variance)*0.6 + 0.25)) %>% 
+  mutate(weather_variance = (sqrt(weather_variance)*0.6 + 0.25)) %>% 
+  mutate(IC_variance = (sqrt(IC_variance)*0.6 + 0.25)) %>% 
+  mutate(parameter_variance = (sqrt(parameter_variance)*0.6 + 0.25)) %>% 
+  mutate(discharge_variance = (sqrt(discharge_variance)*0.6 + 0.25)) 
+  
 # calculate the proportion of variance for all of the uncertainties
 uncert <- uncert %>% mutate(process_prop = process_variance/(process_variance + weather_variance + IC_variance + parameter_variance + discharge_variance)) %>% 
   mutate(weather_prop = weather_variance/(process_variance + weather_variance + IC_variance + parameter_variance + discharge_variance)) %>% 
@@ -284,10 +318,10 @@ dev.off()
 #######################################################################################################################################################################
 # stacked bar plots of proportion variance for each forecast horizon
 # create dataframe to write into inside the loop
-mean_prop <- array(NA, dim = c(16, 6))
+mean_prop <- array(NA, dim = c(14, 6))
 colnames(mean_prop) = c('horizon', 'mean_IC_prop', 'mean_parameter_prop','mean_process_prop','mean_weather_prop', 'mean_discharge_prop')
 
-for (i in 1:16) {
+for (i in 1:14) {
   temp <- uncert_prop[uncert_prop$day_in_future==i,]
   mean_prop[i,1] <- i
   mean_prop[i, 2] = mean(temp$IC_prop, na.rm = TRUE)
@@ -310,7 +344,7 @@ ggplot(mean_prop, aes(x = horizon, y = mean_IC_prop)) +
   geom_bar(stat = 'identity', position = 'stack')
 
 # GET RID OF DAILY FORECASTS AT DAY 15 and 16
-mean_prop_long <- mean_prop_long[mean_prop_long$horizon<15,]
+#mean_prop_long <- mean_prop_long[mean_prop_long$horizon<15,]
 
 cols <- c('IC'="#92D050",'parameter'="#660066", 'process' = '#C55A11', 'weather' = '#FFC000')
 png(paste0(forecast_folder, '/Uncertainty_Bar_AcrossHorizon_Daily.png'), width = 800, height = 885)
@@ -332,8 +366,8 @@ ggplot(mean_prop_long, aes(x = horizon, y = measurement, fill = variable )) +
         legend.text = element_text(size = 30),
         panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
-        panel.border = element_blank())#,
-        #legend.position = 'none')  
+        panel.border = element_blank(),
+        legend.position = 'none')
 dev.off()
 
 # save figure of just the legend
@@ -342,7 +376,7 @@ ggplot(mean_prop_long, aes(x = horizon, y = measurement, fill = variable )) +
   geom_bar(stat = 'identity', position= 'stack') +
   scale_fill_manual(labels = c('discharge', 'initial conditions', 'parameter', 'process', 'meteorological') ,
                     values = c('#4472C4', '#92D050', '#660066', '#C55A11', '#FFC000'),
-                    name = 'Uncertainty Type') +
+                    name = 'Uncertainty Source') +
   xlab('Forecast Horizon (days)') +
   ylab('Proportion of Variance') +
   scale_x_discrete(breaks = c(7,14),
