061c7878dc0143dd25b0642bd4bc0f6c018a5501
wwoel <wwoelmer@vt.edu>
2021-12-06 09:31:11 -0500

    add package::

--- Rscripts/extract_EXOchl_chain_dailyavg.R
+++ Rscripts/extract_EXOchl_chain_dailyavg.R
@@ -7,14 +7,14 @@ extract_chla_chain_dailyavg <- function(fname = catwalk_fname,full_time,depths =
   #d_names <- read.csv(fname, skip =1)
   #names(d) <- names(d_names)
   
-  d$date <- date(d$DateTime)
+  d$date <- lubridate::date(d$DateTime)
   Chla_obs <- array(NA,dim=c(length(full_time),2))
   
   
   for (i in 1:length(full_time)) {
-    temp <- subset(d, d$date==date(full_time)[i])
+    temp <- subset(d, d$date==lubridate::date(full_time)[i])
     temp1 <- temp %>% mutate(chl_avg = mean(EXOChla_ugL_1, na.rm = TRUE))
-    Chla_obs[i,1] <- temp1[1, 48]
+    Chla_obs[i,1] <- temp1$chl_avg[1]
     
        }
   return(list(Chla_obs = Chla_obs, depths = depths))
