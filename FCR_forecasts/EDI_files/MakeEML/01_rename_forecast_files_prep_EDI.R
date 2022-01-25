386052a5be96cb66204ba22172c80c337b52584d
wwoel <wwoelmer@vt.edu>
2022-01-25 15:46:11 -0500

    renaming files for EDI

--- FCR_forecasts/EDI_files/MakeEML/01_rename_forecast_files_prep_EDI.R
+++ FCR_forecasts/EDI_files/MakeEML/01_rename_forecast_files_prep_EDI.R
@@ -17,8 +17,8 @@
 
 #####################################################################################################
 # forecast output files
-new_folder <- './FCR_forecasts/forecast_output_EDI'
-old_folder <- './FCR_forecasts/1day/update_bayes_method_Feb_2021'
+new_folder <- './FCR_forecasts/EDI_files/forecast_output_EDI'
+old_folder <- './FCR_forecasts/1day/Mar2021_UC'
 dir.create(new_folder)
 names <- list.files(path = old_folder, pattern = '*chla_1day.csv')
 newNames <- sub("chla_1day.csv", "chla_1day_uncert1.csv", names)
@@ -29,104 +29,104 @@ file.remove(list.files(path = new_folder, full.names = TRUE))
 file.copy(from = paste0(old_folder, "/", names), to = paste0(new_folder, "/", newNames))
 
 # do again for 1day files in each of the uncertainty folders, uncertainty_2_process
-old_folder <- './FCR_forecasts/1day/update_bayes_method_Oct_2020/uncertainty_2_process'
+old_folder <- './FCR_forecasts/1day/Mar2021_UC/uncertainty_2_process'
 names <- list.files(path = old_folder, pattern = '*chla_1day.csv')
 newNames <- sub("chla_1day.csv", "chla_1day_uncert2.csv", names)
 file.copy(from = paste0(old_folder, "/", names), to = paste0(new_folder, "/", newNames))
 
 # do again for 1day files in each of the uncertainty folders, uncertainty_3_weather
-old_folder <- './FCR_forecasts/1day/update_bayes_method_Oct_2020/uncertainty_3_weather'
+old_folder <- './FCR_forecasts/1day/Mar2021_UC/uncertainty_3_weather'
 names <- list.files(path = old_folder, pattern = '*chla_1day.csv')
 newNames <- sub("chla_1day.csv", "chla_1day_uncert3.csv", names)
 file.copy(from = paste0(old_folder, "/", names), to = paste0(new_folder, "/", newNames))
 
 # do again for 1day files in each of the uncertainty folders, uncertainty_4_initial_condition
-old_folder <- './FCR_forecasts/1day/update_bayes_method_Oct_2020/uncertainty_4_initial_condition'
+old_folder <- './FCR_forecasts/1day/Mar2021_UC/uncertainty_4_initial_condition'
 names <- list.files(path = old_folder, pattern = '*chla_1day.csv')
 newNames <- sub("chla_1day.csv", "chla_1day_uncert4.csv", names)
 file.copy(from = paste0(old_folder, "/", names), to = paste0(new_folder, "/", newNames))
 
 # do again for 1day files in each of the uncertainty folders, uncertainty_5_parameter
-old_folder <- './FCR_forecasts/1day/update_bayes_method_Oct_2020/uncertainty_5_parameter'
+old_folder <- './FCR_forecasts/1day/Mar2021_UC/uncertainty_5_parameter'
 names <- list.files(path = old_folder, pattern = '*chla_1day.csv')
 newNames <- sub("chla_1day.csv", "chla_1day_uncert5.csv", names)
 file.copy(from = paste0(old_folder, "/", names), to = paste0(new_folder, "/", newNames))
 
 # do again for 1day files in each of the uncertainty folders, uncertainty_6_discharge
-old_folder <- './FCR_forecasts/1day/update_bayes_method_Oct_2020/uncertainty_6_discharge'
+old_folder <- './FCR_forecasts/1day/Mar2021_UC/uncertainty_6_discharge'
 names <- list.files(path = old_folder, pattern = '*chla_1day.csv')
 newNames <- sub("chla_1day.csv", "chla_1day_uncert6.csv", names)
 file.copy(from = paste0(old_folder, "/", names), to = paste0(new_folder, "/", newNames))
 
 ##############################
 # do again for 7day forecasts
-old_folder <- './FCR_forecasts/7day/update_bayes_method_Oct_2020'
+old_folder <- './FCR_forecasts/7day/Mar2021_UC'
 names <- list.files(path = old_folder, pattern = '*chla_7day.csv')
 newNames <- sub("chla_7day.csv", "chla_7day_uncert1.csv", names)
 file.copy(from = paste0(old_folder, "/", names), to = paste0(new_folder, "/", newNames))
 
 # do again for 7day files in each of the uncertainty folders, uncertainty_2_process
-old_folder <- './FCR_forecasts/7day/update_bayes_method_Oct_2020/uncertainty_2_process'
+old_folder <- './FCR_forecasts/7day/Mar2021_UC/uncertainty_2_process'
 names <- list.files(path = old_folder, pattern = '*chla_7day.csv')
 newNames <- sub("chla_7day.csv", "chla_7day_uncert2.csv", names)
 file.copy(from = paste0(old_folder, "/", names), to = paste0(new_folder, "/", newNames))
 
 # do again for 7day files in each of the uncertainty folders, uncertainty_3_weather
-old_folder <- './FCR_forecasts/7day/update_bayes_method_Oct_2020/uncertainty_3_weather'
+old_folder <- './FCR_forecasts/7day/Mar2021_UC/uncertainty_3_weather'
 names <- list.files(path = old_folder, pattern = '*chla_7day.csv')
 newNames <- sub("chla_7day.csv", "chla_7day_uncert3.csv", names)
 file.copy(from = paste0(old_folder, "/", names), to = paste0(new_folder, "/", newNames))
 
 # do again for 7day files in each of the uncertainty folders, uncertainty_4_initial_condition
-old_folder <- './FCR_forecasts/7day/update_bayes_method_Oct_2020/uncertainty_4_initial_condition'
+old_folder <- './FCR_forecasts/7day/Mar2021_UC/uncertainty_4_initial_condition'
 names <- list.files(path = old_folder, pattern = '*chla_7day.csv')
 newNames <- sub("chla_7day.csv", "chla_7day_uncert4.csv", names)
 file.copy(from = paste0(old_folder, "/", names), to = paste0(new_folder, "/", newNames))
 
 # do again for 7day files in each of the uncertainty folders, uncertainty_5_parameter
-old_folder <- './FCR_forecasts/7day/update_bayes_method_Oct_2020/uncertainty_5_parameter'
+old_folder <- './FCR_forecasts/7day/Mar2021_UC/uncertainty_5_parameter'
 names <- list.files(path = old_folder, pattern = '*chla_7day.csv')
 newNames <- sub("chla_7day.csv", "chla_7day_uncert5.csv", names)
 file.copy(from = paste0(old_folder, "/", names), to = paste0(new_folder, "/", newNames))
 
 # do again for 7day files in each of the uncertainty folders, uncertainty_6_discharge
-old_folder <- './FCR_forecasts/7day/update_bayes_method_Oct_2020/uncertainty_6_discharge'
+old_folder <- './FCR_forecasts/7day/Mar2021_UC/uncertainty_6_discharge'
 names <- list.files(path = old_folder, pattern = '*chla_7day.csv')
 newNames <- sub("chla_7day.csv", "chla_7day_uncert6.csv", names)
 file.copy(from = paste0(old_folder, "/", names), to = paste0(new_folder, "/", newNames))
 
 # do again for the 14day forecasts
-old_folder <- './FCR_forecasts/14day/update_bayes_method_Oct_2020'
+old_folder <- './FCR_forecasts/14day/Mar2021_UC'
 names <- list.files(path = old_folder, pattern = '*chla_14day.csv')
 newNames <- sub("chla_14day.csv", "chla_14day_uncert1.csv", names)
 file.copy(from = paste0(old_folder, "/", names), to = paste0(new_folder, "/", newNames))
 
 # do again for 14day files in each of the uncertainty folders, uncertainty_2_process
-old_folder <- './FCR_forecasts/14day/update_bayes_method_Oct_2020/uncertainty_2_process'
+old_folder <- './FCR_forecasts/14day/Mar2021_UC/uncertainty_2_process'
 names <- list.files(path = old_folder, pattern = '*chla_14day.csv')
 newNames <- sub("chla_14day.csv", "chla_14day_uncert2.csv", names)
 file.copy(from = paste0(old_folder, "/", names), to = paste0(new_folder, "/", newNames))
 
 # do again for 14day files in each of the uncertainty folders, uncertainty_3_weather
-old_folder <- './FCR_forecasts/14day/update_bayes_method_Oct_2020/uncertainty_3_weather'
+old_folder <- './FCR_forecasts/14day/Mar2021_UC/uncertainty_3_weather'
 names <- list.files(path = old_folder, pattern = '*chla_14day.csv')
 newNames <- sub("chla_14day.csv", "chla_14day_uncert3.csv", names)
 file.copy(from = paste0(old_folder, "/", names), to = paste0(new_folder, "/", newNames))
 
 # do again for 14day files in each of the uncertainty folders, uncertainty_4_initial_condition
-old_folder <- './FCR_forecasts/14day/update_bayes_method_Oct_2020/uncertainty_4_initial_condition'
+old_folder <- './FCR_forecasts/14day/Mar2021_UC/uncertainty_4_initial_condition'
 names <- list.files(path = old_folder, pattern = '*chla_14day.csv')
 newNames <- sub("chla_14day.csv", "chla_14day_uncert4.csv", names)
 file.copy(from = paste0(old_folder, "/", names), to = paste0(new_folder, "/", newNames))
 
 # do again for 14day files in each of the uncertainty folders, uncertainty_5_parameter
-old_folder <- './FCR_forecasts/14day/update_bayes_method_Oct_2020/uncertainty_5_parameter'
+old_folder <- './FCR_forecasts/14day/Mar2021_UC/uncertainty_5_parameter'
 names <- list.files(path = old_folder, pattern = '*chla_14day.csv')
 newNames <- sub("chla_14day.csv", "chla_14day_uncert5.csv", names)
 file.copy(from = paste0(old_folder, "/", names), to = paste0(new_folder, "/", newNames))
 
 # do again for 14day files in each of the uncertainty folders, uncertainty_6_discharge
-old_folder <- './FCR_forecasts/14day/update_bayes_method_Oct_2020/uncertainty_6_discharge'
+old_folder <- './FCR_forecasts/14day/Mar2021_UC/uncertainty_6_discharge'
 names <- list.files(path = old_folder, pattern = '*chla_14day.csv')
 newNames <- sub("chla_14day.csv", "chla_14day_uncert6.csv", names)
 file.copy(from = paste0(old_folder, "/", names), to = paste0(new_folder, "/", newNames))
@@ -135,7 +135,7 @@ file.copy(from = paste0(old_folder, "/", names), to = paste0(new_folder, "/", ne
 # parameter output files
 
 new_folder <- './FCR_forecasts/parameter_output_EDI'
-old_folder <- './FCR_forecasts/1day/update_bayes_method_Oct_2020'
+old_folder <- './FCR_forecasts/1day/Mar2021_UC'
 dir.create(new_folder)
 names <- list.files(path = old_folder, pattern = '*parameters.csv')
 newNames <- sub("parameters.csv", "parameters_1day_uncert1.csv", names)
@@ -146,13 +146,13 @@ file.remove(list.files(path = new_folder, full.names = TRUE))
 file.copy(from = paste0(old_folder, "/", names), to = paste0(new_folder, "/", newNames))
 
 # do again for 7day parameter output
-old_folder <- './FCR_forecasts/7day/update_bayes_method_Oct_2020'
+old_folder <- './FCR_forecasts/7day/Mar2021_UC'
 names <- list.files(path = old_folder, pattern = '*parameters.csv')
 newNames <- sub("parameters.csv", "parameters_7day_uncert1.csv", names)
 file.copy(from = paste0(old_folder, "/", names), to = paste0(new_folder, "/", newNames))
 
 # do again for 14day parameter output
-old_folder <- './FCR_forecasts/14day/update_bayes_method_Oct_2020'
+old_folder <- './FCR_forecasts/14day/Mar2021_UC'
 names <- list.files(path = old_folder, pattern = '*parameters.csv')
 newNames <- sub("parameters.csv", "parameters_14day_uncert1.csv", names)
 file.copy(from = paste0(old_folder, "/", names), to = paste0(new_folder, "/", newNames))
